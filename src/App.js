8c38f43f9c0ca8ccc64a5a7b13be93f699ec527a
Solveig Sif Gudmundsdottir <solveigsif91@gmail.com>
2017-11-04 22:28:04 +0000

    cleaning up commented code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,3 @@
-// import React, { Component } from 'react';
-// import './App.css';
-// import FrontPage from './components/FrontPage'
-//
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <FrontPage/>
-//       </div>
-//     );
-//   }
-// }
-//
-// export default App;
-
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './FrontPage.css';
 import AboutUs from './AboutUs'
 
+
 class FrontPage extends Component {
   render() {
     return (
