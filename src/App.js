ceae7003d455b268435268c9a3a8cf03884ff28c
Solveig Sif Gudmundsdottir <solveigsif91@gmail.com>
2017-11-05 13:08:32 +0000

    fixing console warnings, unused imports and img needed alt

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route} from 'react-router-dom';
 
 import FrontPage from './components/FrontPage.js';
 import AboutUs from './components/AboutUs.js';
@@ -11,7 +11,7 @@ const App = (props) => (
     <div className="App">
       <Route exact path="/" component={FrontPage}/>
       <Route path="/about" component={AboutUs} />
-      <img src={FrontPageCover} className="coverPhoto"/>
+      <img src={FrontPageCover} alt="nyskopunarlandid" className="coverPhoto"/>
     </div>
   </BrowserRouter>
 );
