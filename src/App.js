b3bb8ef69bb811c2b03c4181adc8f3fd75dc4ed4
Solveig Sif Gudmundsdottir <solveigsif91@gmail.com>
2017-11-05 00:43:29 +0000

    making frontpagecover as full screen img on app.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 import FrontPage from './components/FrontPage.js';
 import AboutUs from './components/AboutUs.js';
+import FrontPageCover from './images/FrontPageCover.svg'
 
 const App = (props) => (
   <BrowserRouter>
     <div className="App">
       <Route exact path="/" component={FrontPage}/>
       <Route path="/about" component={AboutUs} />
+      <img src={FrontPageCover} className="coverPhoto"/>
     </div>
   </BrowserRouter>
 );
